3339c0b24f8ab2a39a6b741924ad06b2f1b3e86e
sagini <sir_ghini@syagini.local>
2021-12-13 17:04:32 +0900

    Created a dynamic web server with node.js only

--- one/server.js
+++ one/server.js
@@ -1,26 +1,12 @@
-const path = require('path')
-const os = require('os')
-const {add, subtract, multiply, divide} = require('./math')
+const fs = require('fs');
+const path = require('path');
 
-console.log(add(2,3))
-console.log(subtract(2,3))
-console.log(multiply(2,3))
-console.log(divide(2,3))
+fs.readFile(path.join(__dirname, 'lorem.txt'), (error, data)=>{
+    if ( error ) throw error;
+    console.log(data);
+});
 
-// console.log(__filename)
-// console.log(__dirname)
-// console.log(__basename)
-
-
-// console.log(path.dirname(__filename))
-// console.log(path.basename(__filename))
-// console.log(path.extname(__filename))
-
-
-// console.log(os.homedir())
-// console.log(os.type())
-// console.log(os.version())
-
-
-
-// console.log(path.parse(__filename))
+process.on('uncaughtException', error => {
+    console.error(`There was an uncaught error: ${error}`);
+    process.exit(1);
+})
---
+++ one/server_math.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const os = require('os')
+const {add, subtract, multiply, divide} = require('./math')
+
+console.log(add(2,3))
+console.log(subtract(2,3))
+console.log(multiply(2,3))
+console.log(divide(2,3))
+
+// console.log(__filename)
+// console.log(__dirname)
+// console.log(__basename)
+
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+
+// console.log(os.homedir())
+// console.log(os.type())
+// console.log(os.version())
+
+
+
+// console.log(path.parse(__filename))
